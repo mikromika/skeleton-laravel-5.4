d3cf103daceccd3f38b885531f10344c3bd23840
Mikro Mika <dev.mikromika@gmail.com>
2017-02-13 13:07:47 +0200

    new clean up update

--- sentinel/resources/views/admins/earnings.blade.php
+++
@@ -1,9 +0,0 @@
-
-Admin area
-<li role="presentation">
-    <form action="/logout" method="POST" id="logout-form">
-  {{ csrf_field() }}
-
-  <a href="#" onclick="document.getElementById('logout-form').submit()">logout </a>
-</form>
-</li>
--- sentinel/resources/views/emails/activation.blade.php
+++
@@ -1,2 +0,0 @@
-<h2> Huhuu </h2>
-<p> link <a href="{{env('APP_URL') }}/active{{ $user->email}}/{{ $code }}"> active your account from this link</a> </p>
