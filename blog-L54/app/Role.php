72e12f47ba9ffa8cab3ed63c9ac285e765282fb6
Mikro Mika <dev.mikromika@gmail.com>
2017-02-13 15:42:29 +0200

    updated roles

--- blog-L54/app/Role.php
+++ blog-L54/app/Role.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     const ROLE_ADMIN = 'admin';
-    const ROLE_EDITOR = 'editor';
+    //const ROLE_EDITOR = 'editor';
 
     /**
     * The attributes that are mass assignable.
